bccec04c5a1b5ba04347baaa003ac9bcf3a82de5
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-25 14:21:32 +0200

    Adjust database connection functionality

--- controllers/nyc_eatsControllers.js
+++ controllers/nyc_eatsControllers.js
@@ -1,13 +1,7 @@
 require("dotenv").config();
 const dbString = process.env.DB_STRING,
   { MongoClient } = require("mongodb"),
-  dbName = "sample_restaurants";
-let db;
-
-MongoClient.connect(dbString, { useUnifiedTopology: true }).then((client) => {
-  console.log(`Connected to ${dbName}`);
-  db = client.db(dbName);
-});
+  client = new MongoClient(dbString);
 
 exports.nycEatsIndex = (req, res) => {
   try {
@@ -20,17 +14,23 @@ exports.nycEatsIndex = (req, res) => {
 };
 
 exports.nycEatsSearch = async (req, res) => {
-  let results = await db
-    .collection("restaurants")
-    .find(
-      { borough: req.query.borough, cuisine: req.query.cuisine },
-      { projection: { _id: 0, address: 1, name: 1 } }
-    )
-    .limit(5)
-    .toArray();
-  console.log(results);
-  res.render("./nyc_eats/results.ejs", {
-    results,
-    layout: "./layouts/nyc_eats",
-  });
+  try {
+    const db = client.db("sample_restaurants");
+    let results = await db
+      .collection("restaurants")
+      .find(
+        { borough: req.query.borough, cuisine: req.query.cuisine },
+        { projection: { _id: 0, address: 1, name: 1 } }
+      )
+      .limit(5)
+      .toArray();
+    res.render("./nyc_eats/results.ejs", {
+      results,
+      layout: "./layouts/nyc_eats",
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
 };
