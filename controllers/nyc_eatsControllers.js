af4f84557ffe599acf43754fa961b4d72616bfff
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-29 15:42:38 +0200

    Add filter checkbox and associated styles

--- controllers/nyc_eatsControllers.js
+++ controllers/nyc_eatsControllers.js
@@ -3,6 +3,21 @@ const dbString = process.env.DB_STRING,
   { MongoClient } = require("mongodb"),
   client = new MongoClient(dbString);
 
+/* This does at least something to reduce the number of fast food returns, but it could be better.  There are a lot of variations on Jimbo's and Mcdonald'S, and a better $nor statement would trigger on a partial match.
+        Fortunately I am sure that I will learn this new secret soon. */
+let ffList = [
+  { name: "Mcdonald'S" },
+  { name: "Wendy'S" },
+  { name: "White Castle" },
+  { name: "Burger King" },
+  { name: "Burger King, Popeye'S" },
+  { name: "Jimbo'S Hamburger Palace" },
+  { name: "Jimbos Hamburger Palace" },
+  { name: "Checkers" },
+  { name: "Popeyes Chicken & Biscuits" },
+  { name: "Crown Fried Chicken" },
+];
+
 exports.nycEatsIndex = (req, res) => {
   try {
     res.render("./nyc_eats/index.ejs", {
@@ -15,24 +30,23 @@ exports.nycEatsIndex = (req, res) => {
 
 exports.nycEatsSearch = async (req, res) => {
   try {
+    let dbQuery =
+      req.query.fastFood === "noFF"
+        ? {
+            borough: req.query.borough,
+            cuisine: req.query.cuisine,
+            $nor: ffList,
+          }
+        : {
+            borough: req.query.borough,
+            cuisine: req.query.cuisine,
+          };
     const db = client.db("sample_restaurants");
     let results = await db
       .collection("restaurants")
-      .find(
-        { borough: req.query.borough, cuisine: req.query.cuisine },
-        { projection: { _id: 0, address: 1, name: 1 } }
-      )
+      .find(dbQuery, { projection: { _id: 0, address: 1, name: 1 } })
       .limit(5)
       .toArray();
-    // I did this to try to remove all of the McDonalds et al from the Burgers results, but no luck.  Which is to say, it works, but there are JUST SO MANY.  I was having to increase the document limit up to 100 before I could consistently get results that weren't fast food places.  I would be better off removing them from the DB.
-    // let names = [];
-    // let filteredResults = results.filter((a) => {
-    //   if (!names.includes(a.name)) {
-    //     names.push(a.name);
-    //     return a;
-    //   }
-    // });
-    // results = filteredResults;
     res.render("./nyc_eats/results.ejs", {
       results,
       layout: "./layouts/nyc_eats",
