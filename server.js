647a5daeddc3e47d659ba80362ce106d7a1f1bcd
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-04 16:29:15 +0200

    Add comment

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+// I did the above so that I could get at the subfolders in the /views folder
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
